0c76181e537a175da319a171e579cd8fb61c490c
a-naimankhan <naimakhanaibar@gmail.com>
2026-08-27 01:53:41 +0500

    fix(sql-schema): fixed Orders tables syntax and logical errors also added order_status_history table with index for that

--- order-service/cmd/migrations/000001_init_schema.up.sql
+++ order-service/cmd/migrations/000001_init_schema.up.sql
@@ -1,10 +1,10 @@
-CREATE TABLE Orders (
-    product_id UUID PRIMARY KEY,
-    customer_id UUID not null  REFERENCES users(id) ON DELETE CASCADE,
+CREATE TABLE IF NOT EXISTS Orders (
+    id UUID PRIMARY KEY,
+    customer_id UUID not null,
     status VARCHAR(50) not null ,
     AMOUNT DECIMAL(10, 2) not null ,
     created_at TIMESTAMP NOT NULL DEFAULT NOW() ,
-    updated_at TIMESTAMP NOT NULL DEFAULT NOW() ,
+    updated_at TIMESTAMP NOT NULL DEFAULT NOW()
 );
 
 
@@ -12,5 +12,12 @@ CREATE INDEX idx_orders_customer_id ON Orders(customer_id);
 
 
 
+CREATE TABLE IF NOT EXISTS order_status_history (
+    id UUID PRIMARY KEY,
+    order_id UUID NOT NULL REFERENCES Orders(id) ON DELETE CASCADE,
+    status VARCHAR(50) NOT NULL,
+    comment VARCHAR(512),
+    created_at TIMESTAMP NOT NULL DEFAULT NOW
+);
 
-
+CREATE INDEX idx_order_status_history_order_id ON order_status_history(order_id);
