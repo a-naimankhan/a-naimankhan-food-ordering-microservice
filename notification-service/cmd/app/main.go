43e4f4198d8e3000f9b14753cce14b691b189067
a-naimankhan <naimakhanaibar@gmail.com>
2026-09-04 01:57:55 +0500

    feat(order-service) : initiailized http server on gin with /ping endpoint to test if the app actually works

--- notification-service/cmd/app/main.go
+++ notification-service/cmd/app/main.go
@@ -1,10 +1,17 @@
 package main
 
 import (
+	"context"
+	"log"
+	"net/http"
+	"notification-service/internal/delivery"
 	"notification-service/internal/logger"
 	"os"
 	"os/signal"
 	"syscall"
+	"time"
+
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
@@ -20,6 +27,10 @@ func main() {
 
 	//TODO INIT RABBITMQ CONSUMER
 
+	//TODO INIT HTTP SERVER
+	handler := delivery.NewNotificationHandler()
+	srv := startServer(handler, ":8081", logger)
+
 	//TODO GRACEFULL SHUTDOWN
 	quit := make(chan os.Signal, 1)
 	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
@@ -28,5 +39,43 @@ func main() {
 	<-quit
 
 	//TODO CLEANUP RESOURCES
+	//cleanup http
+
 	logger.Info("Shutdown signal received. Cleaning up resources...")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	logger.Info("Shutting down HTTP server...")
+
+	if err := srv.Shutdown(ctx); err != nil {
+		logger.Error("Server forced to shutdown:", "error", err.Error())
+	} else {
+		logger.Info("HTTP server stopped gracefully")
+	}
+}
+
+func startServer(handler *delivery.NotificationHandler, port string, logger *logger.Logger) *http.Server {
+	r := gin.Default()
+	logger.Debug("STARTED HTTP SERVER")
+	api := r.Group("/api/v1/notification")
+	{
+		api.GET("ping", handler.Ping)
+	}
+
+	logger.Info("HTTP routes registered")
+	logger.Info("Listening on :", "port", 8081)
+
+	srv := &http.Server{
+		Addr:    port,
+		Handler: r,
+	}
+
+	go func() {
+		if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatal("error while starting http server: %s", err)
+		}
+	}()
+	logger.Info("Server started")
+	return srv
+
 }
--- notification-service/internal/delivery/handler.go
+++ notification-service/internal/delivery/handler.go
@@ -1,33 +1,41 @@
 package delivery
 
 import (
-	"context"
-	"log/slog"
-	"notification-service/internal/domain"
+	"net/http"
+	"notification-service/internal/logger"
+
+	"github.com/gin-gonic/gin"
 )
 
 type NotificationHandler struct {
-	logger *slog.Logger
+	log *logger.Logger
 }
 
-func NewNotificationHandler(logger *slog.Logger) *NotificationHandler {
+func NewNotificationHandler() *NotificationHandler {
 	return &NotificationHandler{
-		logger: logger,
+		log: logger.GetLogger(),
 	}
 }
 
-func (h *NotificationHandler) HandleOrderEvent(ctx context.Context, order *domain.OrderEvent) error {
-	h.logger.Debug("Handling order event", "order_id", order.OrderID, "event_type", order.Status)
+//поидее нахуй не нужно
+//func (h *NotificationHandler) HandleOrderEvent(ctx context.Context, order *domain.OrderEvent) error {
+//	h.logger.Debug("Handling order event", "order_id", order.OrderID, "event_type", order.Status)
+//
+//	switch order.Status {
+//	case "created":
+//		h.logger.Info("Sending Notification : Order created", "order_id", order.OrderID, "customer_id", order.CustomerID, "amount", order.Amount)
+//	case "cancelled":
+//		h.logger.Info("Sending Notification : Order cancelled", "order_id", order.OrderID, "customer_id", order.CustomerID, "reason", order.CancelledReason)
+//	default:
+//		h.logger.Warn("Unknown order event type", "order_id", order.OrderID, "event_type", order.Status)
+//	}
+//
+//	//ну пока сервиса конечно нету но с контекстом получается передам туда и еще на счет statuses нужно будет еще правильно спроектировать и сделать чтобы там были все статусы которые есть в order-service
+//	return nil
+//}
 
-	switch order.Status {
-	case "created":
-		h.logger.Info("Sending Notification : Order created", "order_id", order.OrderID, "customer_id", order.CustomerID, "amount", order.Amount)
-	case "cancelled":
-		h.logger.Info("Sending Notification : Order cancelled", "order_id", order.OrderID, "customer_id", order.CustomerID, "reason", order.CancelledReason)
-	default:
-		h.logger.Warn("Unknown order event type", "order_id", order.OrderID, "event_type", order.Status)
-	}
-
-	//ну пока сервиса конечно нету но с контекстом получается передам туда и еще на счет statuses нужно будет еще правильно спроектировать и сделать чтобы там были все статусы которые есть в order-service
-	return nil
+func (h *NotificationHandler) Ping(c *gin.Context) {
+	h.log.Debug("GET /api/v1/ping received", "remote_addr", c.RemoteIP())
+	c.JSON(http.StatusOK, gin.H{"message": "pong"})
+	h.log.Debug("Ping response sent", "status_code", 200)
 }
